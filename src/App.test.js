40a53d581263563767652acb29dc19ed37f9a8db
Hannes Duve <hannes.duve@outlook.com>
2024-01-10 09:52:31 +0700

    add userevent to tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,32 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-test("renders app", () => {
+it("renders app", () => {
   render(<App />);
   const linkElement = screen.getByText(/The Key graphql react app/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test("renders login form", () => {
+it("renders login form", () => {
   render(<App />);
   const linkElement = screen.getByText(/Login Form/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+function setup(jsx) {
+  return {
+    user: userEvent.setup(),
+    // Import `render` from the framework library of your choice.
+    // See https://testing-library.com/docs/dom-testing-library/install#wrappers
+    ...render(jsx),
+  };
+}
+
+xit("user can press login", async () => {
+  const { user } = setup(<App />);
+
+  await user.type(screen.getByLabelText(/username/i), "john");
+  await user.type(screen.getByLabelText(/password/i), "i-am-secret");
+  await user.click(screen.getByRole("button", { name: /submit/i }));
 });
