c72c09251a8f921e725769f29235687bd71f4d1b
Hannes Duve <hannes.duve@outlook.com>
2024-01-19 12:25:24 +0700

    updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
+import { time } from 'console'
 
-it('renders app', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/The Key graphql react app/i)
-  expect(linkElement).toBeInTheDocument()
-})
-
-it('renders login form', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/Login Form/i)
-  expect(linkElement).toBeInTheDocument()
-})
+// SETUP //
 
 function setup(jsx) {
   return {
@@ -23,10 +14,37 @@ function setup(jsx) {
   }
 }
 
-xit('user can press login', async () => {
+// TESTS //
+
+it('renders login form', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/Login Form/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+it('login fails with wrong credentials', async () => {
   const { user } = setup(<App />)
 
   await user.type(screen.getByLabelText(/username/i), 'john')
-  await user.type(screen.getByLabelText(/password/i), 'i-am-secret')
-  await user.click(screen.getByRole('button', { name: /submit/i }))
+  await user.type(screen.getByLabelText(/password/i), 'wrong-password')
+  await user.click(screen.getByRole('button', { name: /Login/i }))
+  // wait 2000ms
+  await new Promise((r) => setTimeout(r, 2000))
+  const errorText = await screen.findByText(/Error/i)
+  expect(errorText).toBeInTheDocument()
+})
+
+xit('login succeeds with correct credentials', async () => {
+  const { user } = setup(<App />)
+
+  // get username and password via environment variables
+  const username = process.env.USERNAME
+  const password = process.env.PASSWORD
+  await user.type(screen.getByLabelText(/username/i), username)
+  await user.type(screen.getByLabelText(/password/i), password)
+  await user.click(screen.getByRole('button', { name: /Login/i }))
+  // wait 2000ms
+  await new Promise((r) => setTimeout(r, 2000))
+  const successText = await screen.findByText(/Hello/i)
+  expect(successText).toBeInTheDocument()
 })
