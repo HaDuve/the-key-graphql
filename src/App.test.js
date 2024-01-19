4cfb10453b4d20a2018e7cfcda2df9550ae2a587
Hannes Duve <hannes.duve@outlook.com>
2024-01-19 12:48:42 +0700

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
+import { act } from 'react-dom/test-utils'
 
 const CONFIDENTIAL = {
-  userName: '<enter username>',
-  password: '<enter password>',
+  // userName: "<enter your username>",
+  // password: "<enter your password>",
 }
 
 // SETUP //
@@ -29,11 +30,19 @@ it('renders login form', () => {
 it('login fails with wrong credentials', async () => {
   const { user } = setup(<App />)
 
-  await user.type(screen.getByLabelText(/username/i), 'john')
-  await user.type(screen.getByLabelText(/password/i), 'wrong-password')
-  await user.click(screen.getByRole('button', { name: /Login/i }))
-  // wait 2000ms
-  await new Promise((r) => setTimeout(r, 2000))
+  await act(
+    async () => await user.type(screen.getByLabelText(/username/i), 'john'),
+  )
+  await act(
+    async () =>
+      await user.type(screen.getByLabelText(/password/i), 'wrong-password'),
+  )
+  await act(
+    async () =>
+      await user.click(screen.getByRole('button', { name: /Login/i })),
+  )
+
+  await new Promise((r) => setTimeout(r, 3000))
   const errorText = await screen.findByText(/Error/i)
   expect(errorText).toBeInTheDocument()
 })
@@ -42,11 +51,26 @@ xit('login succeeds with correct credentials', async () => {
   const { user } = setup(<App />)
 
   // get username and password via environment variables
-  await user.type(screen.getByLabelText(/username/i), CONFIDENTIAL.userName)
-  await user.type(screen.getByLabelText(/password/i), CONFIDENTIAL.password)
-  await user.click(screen.getByRole('button', { name: /Login/i }))
-  // wait 2000ms
-  const successText = await screen.findByText(/Hello/i)
+  await act(
+    async () =>
+      await user.type(
+        screen.getByLabelText(/username/i),
+        CONFIDENTIAL.userName,
+      ),
+  )
+  await act(
+    async () =>
+      await user.type(
+        screen.getByLabelText(/password/i),
+        CONFIDENTIAL.password,
+      ),
+  )
+  await act(
+    async () =>
+      await user.click(screen.getByRole('button', { name: /Login/i })),
+  )
+  await new Promise((r) => setTimeout(r, 3000))
+  const successText = await screen.findByText(/Hello, /i)
   await waitFor(() => {
     expect(successText).toBeInTheDocument()
   })
